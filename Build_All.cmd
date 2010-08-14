3872dfb0d4de46115637dfd14abb115391d0bdaf
Joerg Ruedenauer <j.ruedenauer@gmx.de>
2010-08-14 16:41:44 +0000

    Updated build scripts. Updated change logs.

--- Build_All.cmd
+++ Build_All.cmd
@@ -4,11 +4,13 @@ set installjammer="C:\Program Files (x86)\InstallJammer\installjammer.exe"
 set svn="C:\Program Files (x86)\VisualSVN Server\bin\svn.exe"
 set zip="C:\Program Files (x86)\7-Zip\7z.exe"
 
+call %ant% -DProductVersion=%1
+
 cd Ares.Controller\Ares.Controller\ant
 call %ant% -DProductVersion=%1 clean prepareSetup
 cd ..\..\..
 
-%nant% -D:ProductVersion=%1 clean prepareSetup
+%nant% clean prepareSetup
 
 
 %installjammer% -DProductVersion %1 --build-dir build\temp --build-log-file build\setup.log --output-dir build\output --build-for-release --build Setup\Ares\Ares.mpi
@@ -16,3 +18,5 @@ cd ..\..\..
 %svn% export file:///D:/Repositories/Ares/trunk build\Ares_%1_Source
 cd build
 %zip% a output\Ares_%1_Source.7z Ares_%1_Source
+
+cd ..
