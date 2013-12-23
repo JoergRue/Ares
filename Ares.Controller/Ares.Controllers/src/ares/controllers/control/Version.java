b7a6769dbbce0a6124f2f7e4856b72119384054e
Joerg Ruedenauer <j.ruedenauer@gmx.de>
2013-12-23 16:03:58 +0000

    Updated ChangeLogs and versions.

--- Ares.Controller/Ares.Controllers/src/ares/controllers/control/Version.java
+++ Ares.Controller/Ares.Controllers/src/ares/controllers/control/Version.java
@@ -30,7 +30,7 @@ public class Version implements Comparable<Version> {
 
   private static Version createCurrentVersion() {
     try {
-      return Version.parse("2.3.0"); //$NON-NLS-1$
+      return Version.parse("2.4.0"); //$NON-NLS-1$
     }
     catch (ParseException e) {
       return createVersion(1, 0, 0);
