be38daf201516610c0400f8e4108284bfaedf1d1
Joerg Ruedenauer <j.ruedenauer@gmx.de>
2015-12-26 17:24:00 +0100

    Better fix for Linux Ares Portable: restored missing directories.

--- Build_All.cmd
+++ Build_All.cmd
@@ -31,25 +31,33 @@ cd build
 
 xcopy /S /E /Q /I Ares Ares_Portable
 xcopy /S /E /Q /Y ..\Ares_Portable Ares_Portable
+del /Q Ares_Portable\Music\.gitignore
+del /Q Ares_Portable\Sounds\.gitignore
 rmdir /S /Q Ares_Portable\Player
 rmdir /S /Q Ares_Portable\Player64
 %zip% a output\Ares_Portable_%1.zip Ares_Portable
 
 xcopy /S /E /Q /I Ares Ares_Portable_Linux
-xcopy /S /E /Q /Y ..\Ares_Portable\Player_Editor Ares_Portable_Linux\Player
+xcopy /S /E /Q /Y ..\Ares_Portable Ares_Portable_Linux
+del /Q Ares_Portable\Music\.gitignore
+del /Q Ares_Portable\Sounds\.gitignore
 rmdir /S /Q Ares_Portable_Linux\Player_Editor
 rmdir /S /Q Ares_Portable_Linux\Player64
 %zip% a output\Ares_Portable_Linux_x86_%1.zip Ares_Portable_Linux
 
 xcopy /S /E /Q /I Ares Ares_Portable_Linux_x64
-xcopy /S /E /Q /Y ..\Ares_Portable\Player_Editor Ares_Portable_Linux_x64\Player64
+xcopy /S /E /Q /Y ..\Ares_Portable Ares_Portable_Linux_x64
+del /Q Ares_Portable\Music\.gitignore
+del /Q Ares_Portable\Sounds\.gitignore
 rmdir /S /Q Ares_Portable_Linux_x64\Player_Editor
 rmdir /S /Q Ares_Portable_Linux_x64\Player
 move /Y Ares_Portable_Linux_x64\Player64 Ares_Portable_Linux_x64\Player
 %zip% a output\Ares_Portable_Linux_x64_%1.zip Ares_Portable_Linux_x64
 
 xcopy /S /E /Q /I Ares Ares_Portable_Linux_arm_softfp
-xcopy /S /E /Q /Y ..\Ares_Portable\Player_Editor Ares_Portable_Linux_arm_softfp\Player
+xcopy /S /E /Q /Y ..\Ares_Portable Ares_Portable_Linux_arm_softfp
+del /Q Ares_Portable\Music\.gitignore
+del /Q Ares_Portable\Sounds\.gitignore
 rmdir /S /Q Ares_Portable_Linux_arm_softfp\Player_Editor
 rmdir /S /Q Ares_Portable_Linux_arm_softfp\Player64
 xcopy /S /E /Q /I /Y ..\Libraries_Linux_arm_softfp Ares_Portable_Linux_arm_softfp\Player
@@ -57,7 +65,9 @@ del Ares_Portable_Linux_arm_softfp\Player\libbass_aac.so
 %zip% a output\Ares_Portable_Linux_arm_softfp_%1.zip Ares_Portable_Linux_arm_softfp
 
 xcopy /S /E /Q /I Ares Ares_Portable_Linux_arm_hardfp
-xcopy /S /E /Q /Y ..\Ares_Portable\Player_Editor Ares_Portable_Linux_arm_hardfp\Player
+xcopy /S /E /Q /Y ..\Ares_Portable Ares_Portable_Linux_arm_hardfp
+del /Q Ares_Portable\Music\.gitignore
+del /Q Ares_Portable\Sounds\.gitignore
 rmdir /S /Q Ares_Portable_Linux_arm_hardfp\Player_Editor
 rmdir /S /Q Ares_Portable_Linux_arm_hardfp\Player64
 xcopy /S /E /Q /I /Y ..\Libraries_Linux_arm_hardfp Ares_Portable_Linux_arm_hardfp\Player
