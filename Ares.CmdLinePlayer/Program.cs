4ae6f30223adeb08f45db37c2f61a222c530a4da
Joerg Ruedenauer <j.ruedenauer@gmx.de>
2015-12-26 17:52:36 +0100

    Fix problem with ServiceStack licensing (under Mono) if web port is deactivated.

--- Ares.CmdLinePlayer/Program.cs
+++ Ares.CmdLinePlayer/Program.cs
@@ -29,6 +29,8 @@ namespace Ares.CmdLinePlayer
         {
             System.AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
 
+            ServiceStack.Licensing.RegisterLicense(@"1001-e1JlZjoxMDAxLE5hbWU6VGVzdCBCdXNpbmVzcyxUeXBlOkJ1c2luZXNzLEhhc2g6UHVNTVRPclhvT2ZIbjQ5MG5LZE1mUTd5RUMzQnBucTFEbTE3TDczVEF4QUNMT1FhNXJMOWkzVjFGL2ZkVTE3Q2pDNENqTkQyUktRWmhvUVBhYTBiekJGUUZ3ZE5aZHFDYm9hL3lydGlwUHI5K1JsaTBYbzNsUC85cjVJNHE5QVhldDN6QkE4aTlvdldrdTgyTk1relY2eis2dFFqTThYN2lmc0JveHgycFdjPSxFeHBpcnk6MjAxMy0wMS0wMX0=");
+
             if (Environment.GetCommandLineArgs().Length > 1)
             {
                 string arg = Environment.GetCommandLineArgs()[1];
