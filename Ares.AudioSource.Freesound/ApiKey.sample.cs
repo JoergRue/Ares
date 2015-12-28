45776782837332acc6599cc65f0d9085be3d3bf0
Martin Ried <martin.ried@infomotion.de>
2015-12-28 16:31:24 +0100

    Basic structure for extensible AudioSources and the Freesound AudioSource plugin

---
+++ Ares.AudioSource.Freesound/ApiKey.sample.cs
@@ -0,0 +1,7 @@
+﻿namespace Ares.AudioSource.Freesound
+{
+    class ApiKey
+    {
+        public const string Key = "YOUR_KEY_HERE";
+    }
+}
---
+++ Ares.AudioSource.Freesound/FreesoundAudioSource.cs
@@ -0,0 +1,71 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ares.AudioSource.Freesound
+{
+    public class FreesoundAudioSource
+    {
+        
+
+    }
+
+    public class FreesoundApi
+    {
+        public static String BaseUrl { get { return Settings.Default.FreesoundApiBase; } }
+        public static String TextSearchPath { get { return Settings.Default.TextSearchPath; } }
+    }
+
+    public class FreesoundApiSearch
+    {
+        private Ares.ModelInfo.IProgressMonitor m_Monitor;
+        private CancellationToken m_Token;
+        
+        public FreesoundApiSearch(Ares.ModelInfo.IProgressMonitor monitor, CancellationToken token)
+        {
+            this.m_Monitor = monitor;
+            this.m_Token = token;
+        }
+
+        public void DoSearch(string searchQuery)
+        {
+            m_Monitor.SetIndeterminate(StringResources.ExecutingSearch);
+
+            //var proxy = System.Net.WebRequest.GetSystemWebProxy(); // Unused!
+            var request = new RestSharp.RestRequest(FreesoundApi.TextSearchPath, RestSharp.Method.GET);
+            request.AddParameter("query", searchQuery);
+            request.AddParameter("token",   )
+            var client = new RestSharp.RestClient();
+
+            client.BaseUrl = FreesoundApi.BaseUrl;
+            client.Timeout = 20 * 1000;
+
+            var response = client.Execute<SearchResponse>(request);
+
+            m_Token.ThrowIfCancellationRequested();
+                if (response.ErrorException != null)
+                {
+                    throw new GlobalDbException(response.ErrorException);
+                }
+                if (response.Data == null)
+                {
+                    throw new GlobalDbException(String.IsNullOrEmpty(response.ErrorMessage) ? "No data received" : response.ErrorMessage);
+                }
+                if (response.Data.Status != 0)
+                {
+                    throw new GlobalDbException(response.Data.ErrorMessage);
+                }
+                if (i > 0)
+                {
+                    logBuilder.AppendLine("---------------------------------------------------------");
+                }
+                logBuilder.Append(response.Data.Log);
+            }
+            return logBuilder.ToString();
+        }
+    }
+}
