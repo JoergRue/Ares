65e03947b3f6fc488f27bbf49d92449e02e0bf4d
Joerg Ruedenauer <j.ruedenauer@gmx.de>
2016-01-23 12:11:22 +0100

    Added layouts for landscape and large screen.

--- Ares.Player_Android/Resources/Resource.designer.cs
+++ Ares.Player_Android/Resources/Resource.designer.cs
@@ -283,65 +283,71 @@ namespace Ares.Player_Android
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f080006
-			public const int clearButton = 2131230726;
-			
-			// aapt resource value: 0x7f080011
-			public const int controllerButton = 2131230737;
-			
-			// aapt resource value: 0x7f08000f
-			public const int controllerVIew = 2131230735;
-			
-			// aapt resource value: 0x7f080005
-			public const int filterSpinner = 2131230725;
-			
-			// aapt resource value: 0x7f080002
-			public const int linearLayout1 = 2131230722;
-			
-			// aapt resource value: 0x7f080003
-			public const int linearLayout2 = 2131230723;
-			
 			// aapt resource value: 0x7f080008
-			public const int messageIcon = 2131230728;
-			
-			// aapt resource value: 0x7f080009
-			public const int messageLine = 2131230729;
-			
-			// aapt resource value: 0x7f08000d
-			public const int messagesButton = 2131230733;
-			
-			// aapt resource value: 0x7f080007
-			public const int messagesList = 2131230727;
-			
-			// aapt resource value: 0x7f080001
-			public const int messages_fragment = 2131230721;
+			public const int clearButton = 2131230728;
 			
 			// aapt resource value: 0x7f080013
-			public const int projectView = 2131230739;
+			public const int controllerButton = 2131230739;
 			
-			// aapt resource value: 0x7f08000c
-			public const int serviceButton = 2131230732;
+			// aapt resource value: 0x7f080011
+			public const int controllerVIew = 2131230737;
+			
+			// aapt resource value: 0x7f080007
+			public const int filterSpinner = 2131230727;
+			
+			// aapt resource value: 0x7f080003
+			public const int gridLayout1 = 2131230723;
+			
+			// aapt resource value: 0x7f080004
+			public const int linearLayout1 = 2131230724;
+			
+			// aapt resource value: 0x7f080005
+			public const int linearLayout2 = 2131230725;
 			
 			// aapt resource value: 0x7f08000a
-			public const int serviceView = 2131230730;
+			public const int messageIcon = 2131230730;
+			
+			// aapt resource value: 0x7f08000b
+			public const int messageLine = 2131230731;
+			
+			// aapt resource value: 0x7f080001
+			public const int messageTitleView = 2131230721;
+			
+			// aapt resource value: 0x7f08000f
+			public const int messagesButton = 2131230735;
+			
+			// aapt resource value: 0x7f080009
+			public const int messagesList = 2131230729;
+			
+			// aapt resource value: 0x7f080002
+			public const int messages_fragment = 2131230722;
+			
+			// aapt resource value: 0x7f080015
+			public const int projectView = 2131230741;
+			
+			// aapt resource value: 0x7f08000e
+			public const int serviceButton = 2131230734;
+			
+			// aapt resource value: 0x7f08000c
+			public const int serviceView = 2131230732;
 			
 			// aapt resource value: 0x7f080000
 			public const int service_fragment = 2131230720;
 			
-			// aapt resource value: 0x7f08000b
-			public const int space1 = 2131230731;
-			
-			// aapt resource value: 0x7f080010
-			public const int space2 = 2131230736;
-			
-			// aapt resource value: 0x7f080004
-			public const int textView1 = 2131230724;
-			
-			// aapt resource value: 0x7f08000e
-			public const int textView3 = 2131230734;
+			// aapt resource value: 0x7f08000d
+			public const int space1 = 2131230733;
 			
 			// aapt resource value: 0x7f080012
-			public const int textView5 = 2131230738;
+			public const int space2 = 2131230738;
+			
+			// aapt resource value: 0x7f080006
+			public const int textView1 = 2131230726;
+			
+			// aapt resource value: 0x7f080010
+			public const int textView3 = 2131230736;
+			
+			// aapt resource value: 0x7f080014
+			public const int textView5 = 2131230740;
 			
 			static Id()
 			{
