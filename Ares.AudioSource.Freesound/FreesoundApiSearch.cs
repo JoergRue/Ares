1edf1d00c765d968407f9027bedce24d8261bf67
Joerg Ruedenauer <j.ruedenauer@gmx.de>
2016-07-30 13:09:40 +0200

    Bugfix for audio search returning invalid file paths.

--- Ares.AudioSource.Freesound/FreesoundApiSearch.cs
+++ Ares.AudioSource.Freesound/FreesoundApiSearch.cs
@@ -54,7 +54,18 @@ namespace Ares.AudioSource.Freesound
                 ICollection<ISearchResult> searchResults = new List<ISearchResult>();
                 foreach (SearchResultDtos.Result result in response.Data.results)
                 {
-                    FreesoundApiSearchResult searchResult = new FreesoundApiSearchResult(m_AudioSource, result.previews["preview-hq-mp3"],result.name);
+                    String fileName = result.name;
+                    fileName = fileName.Replace('"', '\'');
+                    foreach (var c in System.IO.Path.GetInvalidPathChars())
+                    {
+                        fileName = fileName.Replace(c, '_');
+                    }
+                    foreach (var c in System.IO.Path.GetInvalidFileNameChars())
+                    {
+                        fileName = fileName.Replace(c, '_');
+                    }
+                    fileName = System.IO.Path.ChangeExtension(fileName, "mp3");
+                    FreesoundApiSearchResult searchResult = new FreesoundApiSearchResult(m_AudioSource, result.previews["preview-hq-mp3"],fileName);
                     searchResult.Title = result.name;
                     searchResult.PreviewUrls = result.previews;
                     searchResult.License = result.license;
